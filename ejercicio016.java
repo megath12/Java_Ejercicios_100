8a732842fadc25ff6ca3d031c8117c3cf60a9238
megath12 <megath@live.com>
2023-12-05 23:49:38 -0600

    ej17

---
+++ ejercicio016.java
@@ -0,0 +1,13 @@
+package ejerciciosJava100;
+
+public class ejercicio016 {
+
+	public static void main(String args[]) {
+		for(int t=0; t<=10; t++) {
+			System.out.println("Number "+t);
+		}
+		System.out.println("Done!");
+	}
+}
+
+// Simple For Loop
---
+++ ejercicio017.java
@@ -0,0 +1,16 @@
+package ejerciciosJava100;
+
+public class ejercicio017 {
+
+	public static void main(String args[]) {
+		for(int f=1; f<=20; f++) {
+			for(int g=1; g<=f; g++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+	
+}
+
+// print star console using Loop
